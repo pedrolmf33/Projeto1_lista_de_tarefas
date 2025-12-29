c91e9718ce2b438280d366d3669305129478c69f
Pedro Luiz de Moraes Ferreira <pedroluiz2070@gmail.com>
2025-12-29 20:27:38 -0300

    feat: atualizando

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,19 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
-import { CirclePlus, List, Check, ClockFading, SquarePen, Trash} from 'lucide-react';
+import { CirclePlus, List, Sigma, Check, ClockFading, SquarePen, Trash, ListChecks} from 'lucide-react';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
 const Home = () => {
   return (
@@ -18,22 +30,66 @@ const Home = () => {
         <Separator className="mb-4"/>
         
         <div className="flex gap-2">
-          <Badge className="cursor-pointer"><List/>Todas</Badge>
-          <Badge className="cursor-pointer"><ClockFading/>Não finalizadas</Badge>
-          <Badge className="cursor-pointer"><Check/>Concluídas</Badge>
+          <Badge className="cursor-pointer" variant="default"><List/>Todas</Badge>
+          <Badge className="cursor-pointer" variant="outline"><ClockFading/>Não finalizadas</Badge>
+          <Badge className="cursor-pointer" variant="outline"><Check/>Concluídas</Badge>
         </div>
 
-        <div className="mt-4">
+        <div className="mt-4 border-b">
           
-          <div className=" h-14 flex justify-between items-center border-b-1 border-t-1">
+          <div className=" h-14 flex justify-between items-center border-t">
             <div className="w-1 h-full bg-green-300"></div>
             <p className="flex-1 px-2 text-sm">Estudar React</p>
-
             <div className="flex gap-2 items-center">
-              <SquarePen size={16} className="cursor-pointer"/>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <SquarePen size={16} className="cursor-pointer"/>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Editar tarefa</DialogTitle>
+                  </DialogHeader>
+
+                  <div className="flex gap-2">
+                    <Input placeholder="Editar tarefa"></Input>
+                    <Button className="cursor-pointer">Editar</Button>
+                  </div>
+
+                </DialogContent>
+              </Dialog>
               <Trash size={16} className="cursor-pointer"/>
             </div>
           </div>
+        </div>
+
+        <div className="flex justify-between mt-4">
+          <div className="flex gap-2 items-center">
+            <ListChecks size={18}/>
+            <p className="text-xs">Tarefas concluídas (3/3)</p>
+          </div>
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button className="text-xs h-7 cursor-pointer" variant="outline"> <Trash></Trash>Limpar tarefas concluídas</Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Tem certeza qeu deseja excluir x itens?</AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogAction>Sim</AlertDialogAction>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+        </div>
+
+        <div className="h-2 bg-gray-100 mt-4 rounded-md">
+          <div className="h-full bg-blue-500 rounded-md" style={{width: "50%"}}></div>
+        </div>
+
+        <div className="flex justify-end items-center mt-2 gap-2">
+          <Sigma size={18}/>
+          <p className="text-xs">3 Tarefas no total</p>
         </div>
 
       </CardContent>
